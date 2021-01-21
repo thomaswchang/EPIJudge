e92facfaaad80b4550e30279236f0b75f65b31e6
thomaswchang <thomas.w.chang@gmail.com>
2021-01-20 22:38:03 -0800

    Add more dp problems

--- epi_judge_python/dynamicprogram/is_string_in_matrix.py
+++ epi_judge_python/dynamicprogram/is_string_in_matrix.py
@@ -2,19 +2,40 @@ from typing import List
 
 from test_framework import generic_test
 
-# [PROBLEM_TYPE=DP]
+# [PROBLEM_TYPE=DP PG 253]
+
+# Given a 2D matrix, and a 1D array of integers pattern, determine if the pattern can be traversed in sequence from the matrix
+
 # Step1: How to recognize a problem is a DP? What is the recurrence?
+# Not sure if this is a DP problem, since there is no optimum solution; it's just recurrence
 
 # Step2: Decide the state. What is the key to the lookup table?
+# state:
+#   current x, y in grid
+#   ith offset to pattern
 
 # Step3: Formulate a relation among the states
 
 # Step4:  Optimize by adding tabulation or memoization
 
+# grid is a square
 def is_pattern_contained_in_grid(grid: List[List[int]],
                                  pattern: List[int]) -> bool:
-    # TODO - you fill in here.
-    return True
+
+    def recurse(x, y, i):
+        if len(pattern) == i:
+            return True
+
+        if not ( (0 <= x <len(grid)) and (0<= y <len(grid[x])) ) or grid[x][y] != pattern[i] :
+            return False
+
+        return any( [recurse(position[0], position[1], i+1) for position in ( (x-1, y), (x+1, y), (x, y-1), (x, y+1))])
+
+
+    return any(
+        [ recurse(r, c, 0) for r in range(len(grid)) for c in range(len(grid[r])) ]
+    )
+
 
 
 if __name__ == '__main__':
--- epi_judge_python/dynamicprogram/number_of_traversals_matrix.py
+++ epi_judge_python/dynamicprogram/number_of_traversals_matrix.py
@@ -1,24 +1,53 @@
 from test_framework import generic_test
 
-# [PROBLEM_TYPE=DP]
+# [PROBLEM_TYPE=DP PG 250]
 
+# Given a matrix, return HOW many different ways of starting from top-left to bottom-right
+#
 
 # Step1: How to recognize a problem is a DP? What is the recurrence?
+# Each position x,y = pos(x-1, y) + pos(x, y-1)
 
 # Step2: Decide the state. What is the key to the lookup table?
+#   state position x,y
+
 
 # Step3: Formulate a relation among the states
 
 # Step4:  Optimize by adding tabulation or memoization
 
 
-def number_of_ways(n: int, m: int) -> int:
-    # TODO - you fill in here.
-    return 0
+def number_of_ways_via_recursion(n: int, m: int) -> int:
+    def recurse(x, y):
+        if x==0 and y==0:
+            return 1
+
+        from_top = 0 if x == 0 else recurse(x-1, y)
+        from_left = 0 if y==0 else recurse(x, y-1)
+
+        return from_top + from_left
+    return recurse(n-1, m-1)
+
+
+def number_of_ways_via_recursion(n: int, m: int) -> int:
+    # grid_incorrect = [[0] * n] * m # * m will create m shallow copies; when I update one row, it will update all 3 rows!!!
+    grid = [[0]*n for _ in range(m)]
+    for r in range(m):
+        for c in range(n):
+            if r==0 or c==0:
+                grid[r][c] = 1
+
+    for r in range(1, m):
+        for c in range(1, n):
+            grid[r][c] = grid[r-1][c] + grid[r][c-1]
+
+    return grid[m-1][n-1]
+
+
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('number_of_traversals_matrix.py',
                                        'number_of_traversals_matrix.tsv',
-                                       number_of_ways))
+                                       number_of_ways_via_recursion))
